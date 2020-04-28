d2a1c24703ffd153d0445612c6315cbd23dc2870
3DPIT <47946136+3DPIT@users.noreply.github.com>
2020-04-28 14:20:08 +0900

    퇴사 문제 풀이완료
    2020.04.28 퇴사문제 풀이

---
+++ Algorithm(2020.04.21)/SwTestPrepare/SwTestPrepare/14501 퇴사.cpp
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<iostream>
+#include<string.h>
+#include<string>
+#include<vector>
+using namespace std;
+#define NS 16
+struct Data {
+	int Ti, Pi;
+};
+vector<Data>s;//스케쥴 저장
+int N;
+int Max = 0x80000000;
+void print(string name, int num) {
+	cout << name << endl;
+	if (name == "N+1위치의sum 값") {
+		cout << num << endl;
+	}
+	if (name == "idx순서") {
+		cout << num << endl;
+	}
+}
+struct Fire {
+	Fire() {
+		scanf("%d", &N);
+		for (int i = 0; i < N; i++) {
+			int day, profit;
+			scanf("%d %d", &day, &profit);
+			s.push_back({ day,profit });
+		}
+		dfs(0, 0);
+		printf("%d\n", Max);
+	}
+	void init() {
+		s.clear();
+		N = 0;
+		Max = 0x80000000;
+	}
+	void dfs(int idx, int sum) {
+		if (idx > N) return;
+		if (idx == N) {
+		//	print("N+1위치의sum 값", sum);
+			Max = Max < sum ? sum : Max;
+			return;
+		}
+		//print("idx순서", idx);
+		dfs(idx + s[idx].Ti, sum + s[idx].Pi);
+		dfs(idx + 1, sum);
+
+	}
+}Fire;
+int main(void) {
+
+	return 0;
+}

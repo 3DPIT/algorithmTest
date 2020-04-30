19d8730aaa14816eae2097b29f795b403eccc351
3DPIT <47946136+3DPIT@users.noreply.github.com>
2020-04-30 18:48:02 +0900

    2020.04.30 사다리 조작
    사다리 조작 문제 완료

---
+++ Algorithm(2020.04.21)/SWTEST/SWTEST/15684 사다리조작.cpp
@@ -0,0 +1,102 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<string>
+#include<string.h>
+using namespace std;
+#define NS 11
+#define HS 101
+int map[HS][NS];
+int N, H, M, ret;
+int Min = 0x7fffffff;
+int Maxcnt = 0;
+void init() {
+	N = H = M = ret = 0;
+	Min = 0x7fffffff;
+	memset(map, 0, sizeof(map));
+}
+void copy(int cmap[HS][NS], int map[HS][NS]) {
+	for (int i = 1; i <= H; i++) {
+		for (int j = 1; j <= N; j++) {
+			cmap[HS][NS] = map[HS][NS];
+		}
+	}
+}
+void print(string name, int idx) {
+	cout << name << endl;
+	if (idx == 0) {
+		for (int i = 0; i <= H; i++) {
+			for (int j = 0; j <= N; j++) {
+				printf("%3d", map[i][j]);
+			}
+			printf("\n");
+		}
+	}
+}
+bool downStart() {
+	int y = 1;
+	int j = 1;
+	int cnt = 0;
+	for (int j = 1; j < N + 1; j++) {
+		y = 0;
+		int yi = j;
+		while (y != H + 1) {
+			if (map[y][j] == 1) {//->
+				y++;
+				j++;
+			}
+			else if (map[y][j - 1] == 1) {//<-
+				y++;
+				j--;
+			}
+			else {
+				y++;
+			}
+		//	cout << y << j << endl;
+		}
+		if (yi != j)return false;
+	//	cout << yi << j << endl;
+	}
+	return true;
+}
+void dfs(int y, int x, int cnt) {
+	if (0<=cnt&&cnt<=3) {
+	//	print("map다리건설", 0);
+		if (downStart()) {
+			if (Min > cnt)Min = cnt;
+		}
+	}
+	if (cnt == 3)return;
+	int cmap[HS][NS] = { 0, };
+	for (int i = y; i <= H; i++) {
+		for (int j = x; j <= N; j++) {
+			copy(cmap, map);
+			if (map[i][j - 1] == 0 && map[i][j + 1] == 0 && map[i][j] == 0) {
+				map[i][j] = 1;
+				dfs(i, j + 1, cnt + 1);
+				map[i][j] = 0;
+			}
+			copy(map, cmap);
+		}
+		x = 1;
+	}
+}
+int main(void) {
+	int T = 1;
+	for (int ti = 1; ti <= T; ti++) {
+		init();
+		scanf("%d %d %d", &N, &M, &H);
+		for (int i = 0; i < M; i++) {
+			int y, x;
+			scanf("%d %d", &y, &x);
+			map[y][x] = 1;
+		}
+	
+			dfs(1, 1, 0);
+		
+		//printf("#%d %d", ti, ret);
+		if (Min == 0x7fffffff)Min = -1;
+		printf("%d\n", Min);
+	}
+	return 0;
+}

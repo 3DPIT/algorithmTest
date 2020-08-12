45b97b28522438a237732009bc9e3196e1b82c66
3DPIT <47946136+3DPIT@users.noreply.github.com>
2020-08-12 22:10:15 +0900

    Update 프로그래머스 주식가격.cpp

--- Algorithm(2020.07.13)/swTestFinal/swTestFinal/swTestFinal/프로그래머스 주식가격.cpp
+++ Algorithm(2020.07.13)/swTestFinal/swTestFinal/swTestFinal/프로그래머스 주식가격.cpp
@@ -20,7 +20,7 @@ vector<int> solution1(vector<int> prices) {
 	vector<int> answer(prices.size());
 	stack<int>s;
 	for (int i = 0; i < prices.size(); i++) {
-		while (!s.empty() && prices[s.top()] < prices[i]) {
+		while (!s.empty() && prices[s.top()] > prices[i]) {
 			answer[s.top()] = i - s.top();
 			s.pop();
 		}
